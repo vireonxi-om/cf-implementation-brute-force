b58ab6404da19c27d8d147687cac8bd9093cf098
rheallyc <vireonxi@gmail.com>
2022-10-07 17:48:31 +0000

    Solve 1535A: Fair Playoff

---
+++ solutions/1535A_Fair_Playoff.cpp
@@ -0,0 +1,26 @@
+// Problem: Fair Playoff
+// URL: https://codeforces.com/problemset/problem/1535/A
+// Rating: 800
+// Tags: brute force, implementation
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int m1, m2, n1, n2;
+
+void solve(){
+    scanf("%d%d%d%d", &m1, &n1, &m2, &n2);
+    if(m1<n1) swap(m1, n1);
+    if(m2<n2) swap(m2, n2);
+    if(m2>n1 && m1>n2) printf("YES\n");
+    else printf("NO\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
+
+
